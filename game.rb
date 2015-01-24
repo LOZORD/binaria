19da44ff7a85ef13ef5cc1125d1f9ce7bba4ed8c
LOZORD <ljrudberg@gmail.com>
2015-01-23 21:00:15 -0600

    Added taxes and end-game checks

--- game.rb
+++ game.rb
@@ -29,19 +29,24 @@ class Game
       holidays[cal_day_today].each do |holiday|
         holiday.celebrate!
       end
-      # TODO: add a daily consumption/update (randomized?)
-      # print the status of the Binarian nation afterwards
-      # SOLUTION: use Projects as daily changes!
+
+      # taxes are collected every quarter
+      if tax_day?
+        collect_taxes!
+      end
+
+      # give an update on current projects
       unless projects.empty?
         puts "### PROJECTS (#{ projects.size }) ###".white_on_green
-        projects.each do |project|
-          project.update!
-        end
-
+        projects.each { |project| project.update! }
         @projects = @projects.compact
       end
 
-      # TODO: check if any end-game/losing conditions are satisfied
+      # check if any end-game/losing conditions are satisfied
+      if lost_game?
+        puts "CONGRATULATIONS, YOU'VE RULED FOR #{ day } DAYS!"
+        break
+      end
 
       puts status.to_s
 
@@ -137,38 +142,6 @@ class Game
     end
   end
 
-  def print_help
-    s = '''
-    After disembowling the previous ruler, the lords and ladies of Binaria
-      have chosen you as the new arbiter of justice and goodwill!
-
-    Although your new job will be pretty sweet, you do have to make tough
-      decisions. Every day, advisors and ambassadors will visit you. We know
-      you\'re more of the fighting type, so we\'ll keep things simple.
-
-    As is the custom, decisions in Binaria are simple Yes or No questions.
-
-    Pretty easy right?
-
-    We\'ve also made your job easier by making each decision an Official
-      Decree. This means that all you need to do is say Yes or No to one
-      question, and the same answer applies to all of the others.
-
-    Press <Enter> to forward through your visitors\' jibber-jabber.
-
-    When you are ready to make your Official Decree,
-      enter in "yes" for Yes, and "no" for No".
-
-    What could go wrong?
-
-    (Press ENTER to continue)
-
-    (Enter QUIT to leave the game)
-    '''
-
-    puts s
-  end
-
   def cal_day_today
     day % Holiday::DAYS_IN_YEAR
   end
@@ -176,4 +149,124 @@ class Game
   def human_cal_day_today
     (cal_day_today + 1).to_s
   end
+
+  private
+    def print_help
+      s = '''
+      After disembowling the previous ruler, the lords and ladies of Binaria
+        have chosen you as the new arbiter of justice and goodwill!
+
+      Although your new job will be pretty sweet, you do have to make tough
+        decisions. Every day, advisors and ambassadors will visit you. We know
+        you\'re more of the fighting type, so we\'ll keep things simple.
+
+      As is the custom, decisions in Binaria are simple Yes or No questions.
+
+      Pretty easy right?
+
+      We\'ve also made your job easier by making each decision an Official
+        Decree. This means that all you need to do is say Yes or No to one
+        question, and the same answer applies to all of the others.
+
+      Press <Enter> to forward through your visitors\' jibber-jabber.
+
+      When you are ready to make your Official Decree,
+        enter in "yes" for Yes, and "no" for No".
+
+      What could go wrong?
+
+      (Press ENTER to continue)
+
+      (Enter QUIT to leave the game)
+      '''
+
+      puts s
+    end
+
+    def lost_game?
+      if status.gold <= 0
+        puts """
+          Sadly, you have depleted Binaria's gold reserves and treasury.
+
+          As such, the common folk and your leal lords have risen up and
+            remove you from your throne and removed your head from your body.
+          """.red
+        true
+      elsif status.rock <= 0
+        puts """
+          Sadly, you have run out of rock with which to build.
+
+          It actually wasn't so bad for a while. That was, until the
+          sharknado came...
+          """.red
+        true
+      elsif status.wood <= 0
+        puts """
+          Sadly, you've run out of wood.
+
+          It might not seem like much. But your navy attempted to build ships
+            with other things, like wood, mud, and goats.
+
+          This was know as the Great Naval Blunder. You have been shamed
+            off of your throne, only to be known as #{ player_name }, the Goat Boat Ruler.
+        """.red
+        true
+      elsif status.food <= 0
+        puts """
+          Sadly, you've run out of food.
+
+          Who knew that starving peasants could operate a guillotine so well?
+       """.red
+        true
+      elsif status.serf_happiness <= 0
+        puts """
+          Sadly, you've pissed off your serfs. They really don't like you.
+
+          Your PR team tried to help, but you really can't put a positive spin
+            on the name \"#{ player_name } The Despicable\"
+        """.red
+        true
+      elsif status.lord_happiness <= 0
+        puts """
+          Sadly, you've disgruntled your leal lords and ladies of Binaria.
+
+          They did not take kindly to this. After a series of Machiavellian
+            and dastardly plans, you've lost your claim to the throne.
+
+          While this is terrible for you, the resulting books, TV shows, and
+            films based on your fall has generated some income for Binaria
+            (+5 gold). In addition, you've been given a fine
+            job at the Binarian Antarctic Research Post cleaning test tubes!
+        """.red
+        true
+      elsif neighbors.all? { |neighbor| neighbor.relation == :enemy }
+        puts """
+          Sadly, you angered all of your neighbors.
+
+          They were always kind of dumb, but they still know how to make
+            pointy sticks. They were also smart enough to rise up against
+            you. You died a hero's death in your war room, playing with
+            action figures. Your enemies only laughed a little bit.
+        """.red
+        true
+      else
+        false
+      end
+    end
+
+    def tax_day?
+      cal_day_today % (Holiday::DAYS_IN_YEAR) == 0
+    end
+
+    def collect_taxes!
+      puts 'HAPPY QUARTERLY TAX DAY!'.green
+      tax_amnt = (status.tax_rate/100.0) + 1
+      temp = status.gold * tax_amnt
+      status.gold += temp.floor
+      if status.tax_rate > 0
+        status.serf_happiness -= 5
+        status.lord_happiness -= 5
+      end
+    end
+  # end private
 end
