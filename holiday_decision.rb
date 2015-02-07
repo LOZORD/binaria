d08f2327ae13e399254211f6335758cf9cda4397
LOZORD <ljrudberg@gmail.com>
2015-02-06 22:04:15 -0600

    Fixed bug with added new holidays to the game's holiday array

--- holiday_decision.rb
+++ holiday_decision.rb
@@ -6,21 +6,18 @@ class HolidayDecision < Decision
     init_obj = {
                   game:         @asker.game,
                   name:         @name,
-                  celebration:  init_obj[:yes][:celebration],
+                  celebration:  init_obj[:yes][:celebration], # TODO make a StatusChange instead
                   day:          @asker.game.cal_day_today
                }
     @new_holiday = Holiday.new(init_obj)
   end
 
   def decide! (choice)
-    result = choice == :yes ? yes : no
-
-    if result == :yes
-      puts "JOYOUS #{ name } TO YOU DEAR LEADER!".bold_green_on_yellow
-      holiday_calendar = asker.game.holidays
-      holiday_calendar[game.cal_day_today] << new_holiday
+    if choice == :yes
+      asker.game.holidays[asker.game.cal_day_today] << new_holiday
     end
 
+    result = (choice == :yes) ? yes : no
     asker.game.status.update_with_change result
 
     @is_decided = true
